c80e2560abed4246287dc3ccf005fa12127e8ca5
JoaoFelipeSL <felipesouza09099@gmail.com>
2025-11-02 20:25:23 +0000

    Desafio Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -5,42 +5,70 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 
+//Função Recursiva do cavalo
+int cavalo() {
+}
+
+//Função recursiva do Bispo
+void bispo(int passos) {
+    if (passos <= 0) {
+        return; 
+    }
+
+    for (int i = 0; i < passos; i++) {
+        printf("Direita, cima\n");
+    }
+
+    bispo(passos - 1);
+}
+//Função recursiva da Torre
+void torre(int casas) {
+    if (casas <= 0) {
+        return; // condição de parada
+    }
+
+    printf("Direita\n");
+    torre(casas - 1); // recursão
+}
+//Função recursiva da Rainha
+void rainha(int casas) {
+    if (casas <= 0) {
+        return; // condição de parada
+    }
+
+    printf("Esquerda\n");
+    rainha(casas - 1); // recursão
+}
+
 
 int main() {
-    //Peça de xadrez Torre
-    int torreCasas = 5;
-    for (torreCasas ; torreCasas > 0 ; torreCasas--) {
-        printf("Direita\n");
-    }
 
     //Peça de xadrez Bispo
-    int bispoCasas = 5;
-    while (bispoCasas > 0)
-    {
-        printf("Direita, cimaz\n");
-        bispoCasas--;
-    }
+    printf("Movimento do Bispo:\n");
+    bispo(5);
+
+    //Peça de xadrez Torre
+    printf("Movimento da Torre:\n");
+    torre(5);
 
     //Peça de xadrez Rainha
-    int rainhaCasas = 8;
-    do
-    {
-        printf("Esquerda\n");
-        rainhaCasas--;
-    } while (rainhaCasas > 0);
+    printf("\nMovimento da Rainha:\n");
+    rainha(8);
 
     //Peça de xadrez Cavalo
+    printf("\nMovimento do Cavalo:\n");
     int cavaloCasas1;
     int cavaloCasas2;
 
     for (int i = 1; i<=2; i++) {
-        printf("Baixo cavalo\n");
+        printf("cima cavalo\n");
         if (i == 2) {
             for (int j = 1; j<=1; j++) {
-                printf("Esquerda cavalo\n");
+                printf("Direita cavalo\n");
             }
             
         }
     }
+
     
 }
